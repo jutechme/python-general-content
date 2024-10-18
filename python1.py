87f50a58a115b76d1dce540cff755a45b5050bb9
jutechme <julia.amandasoa@gmail.com>
2024-10-18 17:47:22 -0300

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,20 @@
+n1 = int(input('digite um numero'))
+n2 = int(input('digite um numero'))
+soma = n1 + n2
+print(soma)
+         
+n1 = int(input('digite um numero'))
+n2 = int(input('digite um numero'))
+subtração = n1 - n2
+print(subtração)
+
+n1 = int(input('digite um numero'))
+n2 = int(input('digite um numero'))
+multiplicação = n1 * n2
+print(multiplicação)
+
+
+n1 = int(input('digite um numero'))
+n2 = int(input('digite um numero'))
+divisão =n1 / n2
+print(divisão)
