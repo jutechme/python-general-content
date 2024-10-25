1d5d882c741f575702f28284f340a4ac77e2db7b
jutechme <julia.amandasoa@gmail.com>
2024-10-25 18:00:05 -0300

    Add files via upload

---
+++ exerciciosaula 8.py
@@ -0,0 +1,51 @@
+#cliente 0
+cliente = str(input('digite seu nome'))
+idade = int (input("digite sua idade"))
+dias = int(input('digite a quantidade de dias da sua estadia'))
+quartos =['duplo','simples','luxo']
+print(quartos)
+precos = [150,100,250]
+print(precos)
+
+
+escolha1 = int(input('escolha o seu quarto'))
+
+if escolha1 == 0:
+   compra = []
+   m_valores = []
+   compra.append(quartos[escolha1])
+   print(compra)
+   m_valores.append(precos[escolha1])
+   t = precos[escolha1] * dias
+   print(t)
+elif escolha1 == 1:
+   compra = []
+   m_valores = []
+   compra.append(quartos[escolha1])
+   print(compra)
+   m_valores.append(precos[escolha1])
+   t = precos[escolha1] * dias
+   print("R$", t)
+elif escolha1 == 2:
+   compra = []
+   m_valores = []
+   compra.append(quartos[escolha1])
+   print(compra)
+   m_valores.append(precos[escolha1])
+   t = precos[escolha1] * dias
+   print(t) 
+   
+
+
+
+
+
+
+##cliente 1
+ # cliente1 = str(input('digite seu nome'))
+# idade1 = int (input("digite sua idade"))
+# escolha2 = str(input('escolha o seu quarto'))
+cliente2
+# cliente2 = str(input('digite seu nome'))
+# idade2 = int (input("digite sua idade"))
+# escolha3 = str(input('escolha o seu quarto'))
